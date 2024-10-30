193a2b6fdf894553d7f9274d007108a23509911b
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-30 16:26:14 -0400

    add ansible null resource for installing ansible on the ami-server

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ resource "aws_instance" "ami" {
     Name= "ami-server"
   }
 }
+resource "null_resource" "ansible" {
+  triggers = {
+    instance = aws_instance.ami.id
+    #triggers only for null resource
+  }
+
+  connection {
+    type        = "ssh"
+    user        = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+    password    = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+    host        = aws_instance.ami.private_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo pip3.11 install ansible hvac",
+    ]
+  }
+
+
+}
