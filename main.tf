5980ab712af2a8729553d608fb6ae26e100bf265
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-30 16:54:52 -0400

    build AMI---golden ami --from the server we created and ansible null resource

--- main.tf
+++ main.tf
@@ -25,5 +25,9 @@ resource "null_resource" "ansible" {
     ]
   }
 
-
+}
+resource "aws_ami_from_instance" "ami" {
+  depends_on         = [null_resource.ansible]
+  name               = "golden-ami-${formatdate("DD-MM-YY", timestamp())}"
+  source_instance_id = aws_instance.ami.id
 }
