13930cc0df614a31bee7cce75c9180dd68eff8fa
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-10-31 09:29:12 -0400

    addd ignore changes for lifecycle so image  if needed then code changes

--- main.tf
+++ main.tf
@@ -26,14 +26,15 @@ resource "null_resource" "ansible" {
   }
 
 }
+
 resource "aws_ami_from_instance" "ami" {
   depends_on         = [null_resource.ansible]
   name               = "golden-ami-${formatdate("DD-MM-YY", timestamp())}"
   source_instance_id = aws_instance.ami.id
-}
-lifecycle {
-  ignore_changes = [
-    name
-    ## Only for one IMAge a Day, if we need a second AMI then code need changes.
-  ]
+  lifecycle {
+    ignore_changes = [
+      name
+      ## Only for one IMAge a Day, if we need a second AMI then code need changes.
+    ]
+  }
 }
